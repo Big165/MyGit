ef5158692f155ea25f18633dc2baa415258a34a9
Big165 <Seagame.w2005@gmail.com>
2025-02-25 11:21:16 +0700

    .

--- home.py
+++ home.py
@@ -6,3 +6,17 @@ st.header("🍖🍖Website Developing using Python🍖🍖")
 
 st.image('./ing/Dog.jpg')
 st.subheader("นายวงศกร สุขขันติราษฎร์")
+
+col1, col2, col3 = st.coiumns(3)
+
+with col1:
+        st.header("black")
+        st.image("./ing/black.jpg")
+
+with col2:
+        st.header("white")
+        st.image("./ing/white.jpg")
+
+with col3:
+        st.header("yellow")
+        st.image("./ing/yollw.jpg")

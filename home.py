58208932b7b67155f6c22e1f0ffdfc44dca126e1
Big165 <Seagame.w2005@gmail.com>
2025-02-25 10:47:49 +0700

    .

--- home.py
+++ home.py
@@ -4,6 +4,5 @@ import pandas as pd
 st.title("🐷🐷🐷Website Developing using Python🐷🐷")
 st.header("🍖🍖Website Developing using Python🍖🍖")
 
-st.image('./img/kairung.jpg')
-st.subheader("Kairung Hengpraprohm")
-
+st.image('./ing/Dog.jpg')
+st.subheader("นายวงศกร สุขขันติราษฎร์")
